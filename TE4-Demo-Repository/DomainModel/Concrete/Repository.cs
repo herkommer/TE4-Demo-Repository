fd1e9c46f0ea102f40d2f59c1890efe6f32e8035
herkommer <m@herkommer.co.uk>
2018-11-05 12:42:49 +0100

    added fake data in repository

--- TE4-Demo-Repository/DomainModel/Concrete/Repository.cs
+++ TE4-Demo-Repository/DomainModel/Concrete/Repository.cs
@@ -9,7 +9,26 @@ namespace TE4_Demo_Repository.DomainModel.Concrete
 {
     class Repository : IRepository
     {
-        public IEnumerable<Car> Cars => throw new NotImplementedException();
+        //borde kopplas till en DB eller liknande
+        //men vi fejkar det tillsvidare
+
+        private List<Car> cars;
+
+        public Repository()
+        {
+            cars = new List<Car>();
+            cars.Add(new Car() { Make = "Volvo", Model = "V70", Color = "Red", Milage = 12900 });
+            cars.Add(new Car() { Make = "Volvo", Model = "V40", Color = "Blue", Milage = 1900 });
+            cars.Add(new Car() { Make = "Volvo", Model = "V60", Color = "Red", Milage = 54900 });
+            cars.Add(new Car() { Make = "Audi", Model = "A3", Color = "Red", Milage = 1250 });
+            cars.Add(new Car() { Make = "Volvo", Model = "XC90", Color = "Black", Milage = 2900 });
+            cars.Add(new Car() { Make = "Volvo", Model = "V70", Color = "Red", Milage = 900 });
+        }
+
+        public IEnumerable<Car> Cars
+        {
+            get { return cars; }
+        }
 
         public void Add(Car c)
         {
--- TE4-Demo-Repository/Form1.cs
+++ TE4-Demo-Repository/Form1.cs
@@ -7,17 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TE4_Demo_Repository.DomainModel.Abstract;
+using TE4_Demo_Repository.DomainModel.Concrete;
 
 namespace TE4_Demo_Repository
 {
     public partial class Form1 : Form
     {
+        private IRepository _repo;
+
         public Form1()
         {
             InitializeComponent();
 
             button1.Text = "Open Add Car";
             button1.Click += new EventHandler(AddCar);
+
+            //visa en lista med bilar
+            _repo = new Repository();
+            listBox1.DataSource = _repo.Cars;
         }
 
         private void AddCar(object sender, EventArgs e)
