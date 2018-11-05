766780432d8b709d9c725a103b7d41bae3687938
herkommer <m@herkommer.co.uk>
2018-11-05 12:35:27 +0100

    added concrete repository

---
+++ TE4-Demo-Repository/DomainModel/Concrete/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TE4_Demo_Repository.DomainModel.Abstract;
+
+namespace TE4_Demo_Repository.DomainModel.Concrete
+{
+    class Repository : IRepository
+    {
+        public IEnumerable<Car> Cars => throw new NotImplementedException();
+
+        public void Add(Car c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
